6c73b7d209d4ca1d421021ac1df1132aa125fa6e
aphedges <aphedges1@gmail.com>
2017-12-07 20:21:13 -0500

    Modified config file to work better for us

--- config.php
+++ config.php
@@ -12,19 +12,19 @@ $aspect ="m169";
 
 // ##### Date / Timezone #####
 // You can safely disable my default timezone by adding // to the start of the line.
-date_default_timezone_set("Asia/Tokyo"); 
+date_default_timezone_set("America/New York"); 
 // You do not need to edit this unless you know what you are doing.
-$date= strtoupper(date("M jS l", time()));
+$date= strtoupper(date("l, M j, Y", time()));
 
 // ##### Weather #####
 // see weather.js file
 // weather location - examples: "Manchester, England", "Paris, Texas", "Paris, France"
 // or check on Yahoo weather if you have trouble,
-$wloc="Nanjo-shi";
+$wloc="Rochester, New York";
 // weather forecast days, MAX 9
 $wdays=4;
 // Temperature units; C or F
-$wunit = "C";
+$wunit = "F";
 
 // ##### IP Checker, 0=off 1=on #####
 $ipchecker=0;
@@ -45,52 +45,12 @@ $ip="153.189.6.28";
 // mus = 0 or 1. mus=0 channels will switch between each other, as will mus=1, but they wont cross over unless you manually click.
 // Its is designed to have MUSIC separate from other content. So you can put the Kitchen TV into music mode.
 
-// Sky News
-$streams[]=array(
-    "name" => "SN",
-    "url" => "y60wDzZt8yg",
-    "time" => 1200,
-    "mus" => 0
-);
-
-// Monstercat Music
-$streams[]=array(
-    "name" => "MC",
-    "url" => "cCmJdLA5b1I",
-    "time" => 1200,
-    "mus" => 1
-);
-
-// France 24 News
-$streams[]=array(
-    "name" => "FR",
-    "url" => "Fwxuzl4ZrHo",
-    "time" => 1200,
-    "mus" => 0
-);
-
-// Vibe Guide
-$streams[]=array(
-    "name" => "VG",
-    "url" => "Ww5wO_lxOSk",
-    "time" => 1200,
-    "mus" => 1
-);
-
 // Nasa live
 $streams[]=array(
     "name" => "NS",
     "url" => "RtU_mdL2vBM",
     "time" => 300,
     "mus" => 0
-);
-
-// No Copyright Sounds
-$streams[]=array(
-    "name" => "NC",
-    "url" => "1-AODuJpCG4",
-    "time" => 1200,
-    "mus" => 1
 );
 
 // Some channels change their url frequently, so watch out for that & pick what works for you.
@@ -101,14 +61,14 @@ $streams[]=array(
 // this works off <title> <description> & <link> so those elements must be present in the XML doc
 
 //$news_url="http://www.aljazeera.com/xml/rss/all.xml";
-$news_url="http://feeds.bbci.co.uk/news/rss.xml?edition=uk";
-//$news_url="http://feeds.bbci.co.uk/news/world/us_and_canada/rss.xml";
+//$news_url="http://feeds.bbci.co.uk/news/rss.xml?edition=uk";
+$news_url="http://feeds.bbci.co.uk/news/world/us_and_canada/rss.xml";
 //$news_url="https://news.google.com/news?cf=all&hl=en&pz=1&ned=us&output=rss";
 //$news_url="http://www.techradar.com/rss";
 
 // ###### Weather Underground alerts ####
 // your local weather station page to extract alerts from
-$wupage="https://www.wunderground.com/q/zmw:90001.1.99999";
+$wupage="https://www.wunderground.com/weather/us/ny/rochester/14623";
 // uncomment "wu_advisory" on index.php to start using
 
 
